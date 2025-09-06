c2391af0a1f797b685bd4d6e34db74d5332a74fd
JeffM20501 <jeff.muna@student.moringaschool.com>
2025-09-06 15:01:45 +0300

    added smooth scrool
    Co-Authored-By: jeff20501 <148542805+jeff20501@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const contactEl = document.getElementById("contact")
 const contactBtnEl = document.querySelectorAll(".contactbtn")
 
-const scroll=()=>{
-    contactBtnEl.forEach(button=>{
-        button.addEventListener('click', button.scrollIntoView({behaviour:"smooth"}))
+contactBtnEl.forEach(button=>{
+    button.addEventListener('click', ()=>{
+        contactEl.scrollIntoView({behavior:"smooth"})
     })
-}
+})
