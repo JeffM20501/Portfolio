d88e735feee7979af804f93f09be8ac705d3883b
JeffM20501 <jeff.muna@student.moringaschool.com>
2025-09-06 14:53:37 +0300

    added classes to contact buttons
    Co-Authored-By: jeff20501 <148542805+jeff20501@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-const contact = document.getElementById("contact")
+const contactEl = document.getElementById("contact")
+const contactBtnEl = document.querySelectorAll(".contactbtn")
+
+const scroll=()=>{
+    contactBtnEl.forEach(button=>{
+        button.addEventListener('click', button.scrollIntoView({behaviour:"smooth"}))
+    })
+}
